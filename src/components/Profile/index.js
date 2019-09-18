43f0a25e7231dcd98ab660d400393c09ae9d17fc
Matheus Quintaes <matheus.quintaes@gmail.com>
2019-09-18 20:30:15 +1200

    Layout and Styled components

---
+++ src/components/Profile/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Matheus Quintaes .</h1>
+    <h2>Front-end Ux/UI</h2>
+  </div>
+)
+
+export default Profile
