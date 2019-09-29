05ad368d38a9c67aa26c445da385d7b0e2590dba
Matheus Quintaes <matheus.quintaes@gmail.com>
2019-09-29 17:36:59 +1300

    Mobile corrections

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,11 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <h1>About</h1>
-    <p>Matheus</p>
+    <p>Front-end UX/UI developer with 5 years experience in web developing focusing in usability, semantics, accessibility and clean code.
+Experience in application development using Javascript, HTML, CSS, SASS, React, Graphql.
+Since I'm a non-stop learner, I'm always looking for development of new technologies, industry trends and standardization. Ability to work in multidisciplinary teams to define new functionalities.</p>
+
+      
   </Layout>
 )
 
